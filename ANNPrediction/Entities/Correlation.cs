637cdca24906b1a4653825f99ccbf6c6442d010b
khachung225@gmail.com <khachung225@gmail.com>
2014-09-26 00:29:10 +0700

    them file

---
+++ ANNPrediction/Entities/Correlation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ANNPrediction.Entities
+{
+    public class Correlation
+    {
+        private double Tx { get; set; }
+        private double Ty { get; set; }
+
+        private List<CorPoint> listpiont = new List<CorPoint>();
+        public void Add(PredictionResults p)
+        {
+            listpiont.Add(new CorPoint(p.ActualValue,p.PredictedValue));
+            Tx += p.ActualValue;
+            Ty += p.PredictedValue;
+        }
+        public void Add(List<PredictionResults> p)
+        {
+            foreach (var predictionResultse in p)
+            {
+                Add(predictionResultse);
+            }
+        }
+        public void Clean()
+        {
+            listpiont.Clear();
+            Tx = 0;
+            Ty = 0;
+        }
+        public double GetCorrelation()
+        {
+            var cout = listpiont.Count;
+            var tbx = Tx/cout;
+            var tby = Ty/cout;
+            double tuso = 0;
+            double mauso1 = 0;
+            double mauso2 = 0;
+            foreach (var corPoint in listpiont)
+            {
+                var a = corPoint.X - tbx;
+                var b = corPoint.Y - tby;
+
+                tuso += a*b;
+                mauso1 += a*a;
+                mauso2 += b*b;
+            }
+            if (mauso1 != 0 && mauso2 != 0)
+            {
+                return tuso/Math.Sqrt(mauso1*mauso2);
+            }
+            return 0;
+        }
+    }
+    public class CorPoint
+{
+        public CorPoint(){}
+        public CorPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+}
+}
---
+++ ANNPrediction/Entities/StaticReporting.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ANNPrediction.Entities
+{
+    public class StaticReporting
+    {
+        private List<double> _listError = new List<double>();
+        private double _min = double.MaxValue;
+        private double _max = double.MinValue;
+        private double _totalVaule = 0;
+
+
+        public void Add(double p)
+        {
+             _listError.Add(p);
+            if (_max < p)
+                _max = p;
+            if (_min > p)
+                _min = p;
+
+            _totalVaule += p;
+        }
+        public void Add(List<PredictionResults> p)
+        {
+            foreach (var predictionResultse in p)
+            {
+                Add(predictionResultse.Error);
+            }
+        }
+        public void Clean()
+        {
+            _listError.Clear();
+            _min = double.MaxValue;
+            _max = double.MinValue;
+            _totalVaule = 0;
+        }
+
+        public double Max { get { return _max; } }
+        public double Min { get { return _min; } }
+
+        public double GetMVD()
+        {
+            if (_listError.Count == 0) return double.MinValue;
+            return _totalVaule/_listError.Count;
+        }
+        public double GetSDD()
+        {
+            if (_listError.Count == 0) return double.MinValue;
+            var a = GetMVD();
+            double tl = 0;
+            foreach (var d in _listError)
+            {
+                tl += Math.Pow((d - a), 2);
+            }
+            return Math.Sqrt(tl/_listError.Count);
+        }
+        public double GetMDD()
+        {
+            if (_listError.Count == 0) return double.MinValue;
+            var a = GetMVD();
+            double tl = 0;
+            foreach (var d in _listError)
+            {
+                tl += Math.Abs(d - a);
+            }
+            return tl / _listError.Count;
+        }
+    }
+}
